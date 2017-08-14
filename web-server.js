2650536ea233262e85b5bc089f1e7e2c32e27440
Ravinder Singh <rsingh@ravi-singh.com>
2017-08-14 11:22:00 +0100

    Comments and request inspection.

--- web-server.js
+++ web-server.js
@@ -1,12 +1,22 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
+
+// the function passed in to createServer (request handler)
+// is called once for every HTTP request 
 const server = http.createServer((req, res) => {
+    
+    // inspect request
+    console.log("Request method", req.method);
+    console.log("Request url", req.url);
+
+    // response
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
 });
 
+// start web server/listen for requests
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
