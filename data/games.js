b81bef6f60284262f9fd2dc6647f95bc896009c7
Ravinder Singh <rsingh@ravi-singh.com>
2017-08-14 15:20:38 +0100

    Added module export/import example.

---
+++ data/games.js
@@ -0,0 +1,7 @@
+const data = [
+    { name: 'Metal Gear Solid', platforms: ['Playstation'] },
+    { name: 'Need for Speed', platforms: ['Playstation, Xbox, PC'] },
+    { name: 'Microsoft Flight Simulator FSX', platforms: ['PC'] },
+];
+
+module.exports.data = data;
