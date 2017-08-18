6f520056471d352c8682471199295d8018a81de3
Ravinder Singh <rsingh@ravi-singh.com>
2017-08-18 13:09:13 +0100

    Started ES2015 example script

---
+++ es2015-features.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// ES2015 (previously known as ES6) feature examples
+
+if (true) {
+    var brand = 'Ford';
+}
+
+// variable brand gets hoisted and is accessible
+// outside of the if block scope
+console.log(brand);
+
+if (true) {
+    let model = 'Fiesta';
+}
+
+// variables declared with 'let' and 'const' do not
+// get hoisted, variable model is not accessible here,
+// the following commented line results in: ReferenceError (model is not defined)
+// console.log(model);
+
+const PI = 3.142;
+
+// const can't be assigned to multiple times, the following lines
+// results in: TypeError: Assignment to constant variable
+//PI = 3.14159265359
